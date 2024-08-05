3c36a3f965b9c5b478f5ecc5b2afb5eceb022043
aaa123ccc <fakekalilenks@gmail.com>
2024-08-05 15:49:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleLogin() {
+    // تعيين عنوان URL للتوجيه إلى حساب إنستجرام المحدد
+    window.location.href = "https://www.instagram.com/sally__733?igsh=dmRlNDVzZjlnaGZ3";
+    // إرجاع قيمة false لمنع تقديم النموذج
+    return false;
+}
